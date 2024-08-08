816f5bb6eaa3246ae809ed5157b70fb01d92d68d
Mustaqeem Momin <97268809+Mustaqeem1021@users.noreply.github.com>
2024-08-08 19:22:40 +0530

    Create 08 - Sum Tree.java

---
+++ August 2024/08 - Sum Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean isSumTree(Node root) {
+        // Your code here
+        return helper(root) != 0;
+    }
+    
+    private int helper(Node root){
+        if(root == null) return 0;
+        if(root.left == root.right) return root.data;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(root.data != left+right){
+            return 0;
+        }
+        return root.data+left+right;
+    }
+    
+}
+
