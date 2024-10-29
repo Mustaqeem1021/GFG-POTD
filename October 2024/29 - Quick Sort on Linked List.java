da9e92b5606da1d8dc028b875c7dea2d731d7a3f
Mustaqeem Momin <97268809+Mustaqeem1021@users.noreply.github.com>
2024-10-29 11:58:57 +0530

    Update 29 - Quick Sort on Linked List.java

--- October 2024/29 - Quick Sort on Linked List.java
+++ October 2024/29 - Quick Sort on Linked List.java
@@ -1,19 +1,41 @@
 class GfG {
-    public static Node quickSort(Node node) {
-        // Your code here
-        if(node==null || node.next==null) return node;
-        ArrayList<Integer>li=new ArrayList<>();
-        Node cur=node;
-        while(cur!=null){
-            li.add(cur.data);
-            cur=cur.next;
+    public static Node quickSort(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node[] smaller = new Node[1];
+        Node[] greater = new Node[1];
+
+        partition(head, smaller, greater);
+
+        smaller[0] = quickSort(smaller[0]);
+        greater[0] = quickSort(greater[0]);
+
+        head.next = greater[0];
+        Node temp = smaller[0];
+        while (temp != null && temp.next != null)
+            temp = temp.next;
+
+        if (temp != null)
+            temp.next = head;
+        else
+            smaller[0] = head;
+
+        return smaller[0];
+    }
+
+    private static void partition(Node pivot, Node[] smaller, Node[] greater) {
+        Node temp = pivot.next;
+        while (temp != null) {
+            Node next = temp.next;
+            if (temp.data <= pivot.data) {
+                temp.next = smaller[0];
+                smaller[0] = temp;
+            } else {
+                temp.next = greater[0];
+                greater[0] = temp;
+            }
+            temp = next;
         }
-        Collections.sort(li);
-        cur=node;
-        for(int i=0;i<li.size();i++){
-            cur.data=li.get(i);
-            cur=cur.next;
-        }
-        return node;
     }
 }
