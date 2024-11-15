bff51f0d07d0236a6f382e38fd0ba3ef6b3ddfa3
Mustaqeem Momin <97268809+Mustaqeem1021@users.noreply.github.com>
2024-11-15 19:54:50 +0530

    Create 15 - Second Largest.java

---
+++ November 2024/15 - Second Largest.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        HashSet<Integer> ll=new HashSet<Integer>();
+        for(int i=0;i<arr.length;i++){
+            ll.add(arr[i]);
+        }
+        if(ll.size()==1){
+            return -1;
+        }
+         ArrayList<Integer> list = new ArrayList<Integer>(ll); 
+         Collections.sort(list);
+         return list.get(list.size()-2);
+    }
+}
