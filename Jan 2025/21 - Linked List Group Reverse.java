9e737f9367f2a86abad4f69e32659b3863e79376
Mustaqeem Momin <97268809+Mustaqeem1021@users.noreply.github.com>
2025-01-21 22:27:00 +0530

    Create 21 - Linked List Group Reverse.java

---
+++ Jan 2025/21 - Linked List Group Reverse.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static Node reverseKGroup(Node head, int k) {
+        // code here
+        Node outerTemp = head;
+        Node innerTemp = head;
+        Node connectingNode = null;
+        Node newHead = null;
+        
+        while(outerTemp != null){
+            int i = k;
+            while(i > 0 && innerTemp != null){
+                innerTemp = innerTemp.next;
+                i--;
+            }
+            
+            Node curr = outerTemp;
+            Node nextNode = curr;
+            Node prev = null;
+            while(curr != innerTemp){
+                nextNode = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextNode;
+            }
+            
+            if(newHead == null) newHead = prev;
+            
+            if(connectingNode != null){
+                connectingNode.next = prev;
+            }
+            
+            connectingNode = outerTemp;
+            outerTemp = innerTemp;
+        }
+        
+        return newHead;
+    }
+}
